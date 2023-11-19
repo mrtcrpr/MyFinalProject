07980adb2d8ee29b9177d2d089de79680df38606
Murat Çarpar <mrt.crpr@gmail.com>
2023-11-19 15:24:51 +0300

    Added WebAPI and Core

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,18 +1,45 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach(var product in productManager.GetByUnitPrice(40, 100))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+
             }
         }
     }
